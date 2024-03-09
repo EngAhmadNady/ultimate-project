b0ea7bbe83ece67d9a5a9ff306b98daf9d78666b
EngAhmadNady <engahmadnady71@gmail.com>
2024-03-09 22:23:46 +0200

    steps from 12 to 14

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,1 @@
-$(function(){"use strict";$(".toggle-sidebar").on("click",function(){$(".content-area").toggleClass("no-sidebar"),$(".sidebar").toggleClass("no-sidebar")})});
+$(function(){"use strict";$(".toggle-sidebar").on("click",function(){$(".content-area").toggleClass("no-sidebar"),$(".sidebar").toggleClass("no-sidebar")}),$(".toggle-submenu").on("click",function(){$(this).find(".fa-chevron-right").toggleClass("down"),$(this).next(".child-links").slideToggle()})});
--- stage/js/main.js
+++ stage/js/main.js
@@ -4,4 +4,9 @@ $(function(){
         $(".content-area").toggleClass('no-sidebar');
         $(".sidebar").toggleClass('no-sidebar');
     });
+    //toggle-submenu
+    $(".toggle-submenu").on('click',function(){
+        $(this).find(".fa-chevron-right").toggleClass("down");
+        $(this).next(".child-links").slideToggle();
+    });
 });
