16cb2734b29a401b05632c1040eb7eccfa988fec
EngAhmadNady <engahmadnady71@gmail.com>
2024-03-10 15:56:55 +0200

    step 18

--- stage/js/main.js
+++ stage/js/main.js
@@ -9,4 +9,44 @@ $(function(){
         $(this).find(".fa-chevron-right").toggleClass("down");
         $(this).next(".child-links").slideToggle();
     });
+    //open & close fullscreen
+    $('.toggle-fullscreen').on('click', function () {
+        $(this).toggleClass("fullscreen");
+        if($(this).hasClass("fullscreen")){
+            openFullscreen();
+        }else{
+            closeFullscreen();
+        }
+
+    });
 });
+
+
+
+
+//open fullscreen
+var elem = document.documentElement;
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  }
+}
+//close fullscreen
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
